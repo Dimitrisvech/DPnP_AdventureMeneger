56dbb21f563019fad9722d6824c2382d096b8eee
Dima S <dima.svech@gmail.com>
2016-05-12 12:58:21 +0300

    Added next screen - View character details and save - Not implemented yet

---
+++ DigitalPanAndPaper_AdventureManager/app/src/main/java/com/digitalpanandpaper/www/digitalpanandpaper_adventuremanager/CharacterViewActivity.java
@@ -0,0 +1,13 @@
+package com.digitalpanandpaper.www.digitalpanandpaper_adventuremanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CharacterViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_view);
+    }
+}
