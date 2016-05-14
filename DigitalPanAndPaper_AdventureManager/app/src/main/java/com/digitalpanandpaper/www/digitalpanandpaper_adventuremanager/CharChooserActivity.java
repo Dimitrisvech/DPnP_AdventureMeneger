7051cc6db9f7c62cb8bcfc06da2ffc0d1083485d
Dima S <dima.svech@gmail.com>
2016-05-14 16:20:36 +0300

    Added confirmation and toast massage to deleting.

--- DigitalPanAndPaper_AdventureManager/app/src/main/java/com/digitalpanandpaper/www/digitalpanandpaper_adventuremanager/CharChooserActivity.java
+++ DigitalPanAndPaper_AdventureManager/app/src/main/java/com/digitalpanandpaper/www/digitalpanandpaper_adventuremanager/CharChooserActivity.java
@@ -1,7 +1,9 @@
 package com.digitalpanandpaper.www.digitalpanandpaper_adventuremanager;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -22,6 +25,7 @@ public class CharChooserActivity extends AppCompatActivity {
     private IDataAgent _dataAgent;
     private ArrayList<Character> _characterList;
     private LinearLayout _container;
+    private final Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -100,8 +104,31 @@ public class CharChooserActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                removeChar(characterName);
-                ((LinearLayout)charView.getParent()).removeView(charView);
+                //*************************
+                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which){
+                            case DialogInterface.BUTTON_POSITIVE:
+                                //Do your Yes progress
+                                removeChar(characterName);
+                                ((LinearLayout)charView.getParent()).removeView(charView);
+                                String deletedMsg = characterName+" has been deleted.";
+                                Toast.makeText(CharChooserActivity.this, deletedMsg, Toast.LENGTH_SHORT).show();
+                                
+                                break;
+
+                            case DialogInterface.BUTTON_NEGATIVE:
+                                //Do your No progress
+                                break;
+                        }
+                    }
+                };
+                AlertDialog.Builder ab = new AlertDialog.Builder(context);
+                ab.setMessage("Are you sure you want to delete?").setPositiveButton("Yes", dialogClickListener)
+                        .setNegativeButton("No", dialogClickListener).show();
+                //*************************
+
             }});
         _container.addView(charView);
     }
